f4bcec0471be8671f2f7f0baf550f2376b0ee87b
Sahumayank2006 <mayank.sahu1@s.amity.edu>
2026-07-18 18:29:50 +0530

    Commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhLoctz_s70QKG8U3HlcDIjBPJSe1tAAo",
-  authDomain: "kramix-7375e.firebaseapp.com",
-  projectId: "kramix-7375e",
-  storageBucket: "kramix-7375e.firebasestorage.app",
-  messagingSenderId: "705873254180",
-  appId: "1:705873254180:web:9f706c505cf405736f45b1",
-  measurementId: "G-ZKZYWNCZR0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
